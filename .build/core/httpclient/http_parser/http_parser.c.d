3b249732269c29314d7a6901037e0310724cb428
bluejazzCHN <songjiangzhang@hotmail.com>
2019-11-23 13:24:47 +0800

    first demo is OLED Display include printing string and drawing picture.

---
+++ .build/core/httpclient/http_parser/http_parser.c.d
@@ -0,0 +1,4 @@
+d:\WorkFolder\Hardware\aziot\SDK_Demo\.build\core\httpclient\http_parser\http_parser.c.o: \
+ C:\Users\SJ\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.6.3\cores\arduino\httpclient\http_parser\http_parser.c \
+ C:\Users\SJ\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.6.3\system/mbed_config.h \
+ C:\Users\SJ\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.6.3\cores\arduino\httpclient\http_parser\http_parser.h
---
+++ .build/core/pgmspace.cpp.d
@@ -0,0 +1,4 @@
+d:\WorkFolder\Hardware\aziot\SDK_Demo\.build\core\pgmspace.cpp.o: \
+ C:\Users\SJ\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.6.3\cores\arduino\pgmspace.cpp \
+ C:\Users\SJ\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.6.3\system/mbed_config.h \
+ C:\Users\SJ\AppData\Local\Arduino15\packages\AZ3166\hardware\stm32f4\1.6.3\cores\arduino\pgmspace.h
